e54bad4983896a29a1715945bd0e629c56a4680b
LuisSMelchor <luissmmelchor@gmail.com>
2025-07-30 13:09:17 -0400

    Delete netlify/functions/debug.js

--- netlify/functions/debug.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-    ALL_ENV: Object.keys(process.env)
-  })
-});
-
