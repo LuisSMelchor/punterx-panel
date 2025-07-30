4273a91fe7b948c034dc6e0ac14e5634c7eddfdc
LuisSMelchor <luissmmelchor@gmail.com>
2025-07-30 13:06:23 -0400

    Update debug.js

--- netlify/functions/debug.js
+++ netlify/functions/debug.js
@@ -1,1 +1,9 @@
+exports.handler = async () => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    ALL_ENV: Object.keys(process.env)
+  })
+});
 
