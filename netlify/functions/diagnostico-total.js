1a191d597fa6b2f468745af3f9736acb6d6441ad
LuisSMelchor <luissmmelchor@gmail.com>
2025-08-07 19:53:30 -0400

    Update diagnostico-total.js

--- netlify/functions/diagnostico-total.js
+++ netlify/functions/diagnostico-total.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_KEY
 );
 
 exports.handler = async () => {
