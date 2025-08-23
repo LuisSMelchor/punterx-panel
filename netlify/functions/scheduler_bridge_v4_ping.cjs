bbdb91864de21bac991f11f5731d20b3f243f304
LuisSMelchor <you@example.com>
2025-08-23 21:40:12 +0000

    fix: clean netlify.toml (single run2 schedule) + run2 wrapper adjustments

--- netlify/functions/scheduler_bridge_v4_ping.cjs
+++
@@ -1,6 +0,0 @@
-'use strict';
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'content-type': 'application/json' },
-  body: '{"ok":true,"ping":"scheduler_bridge_v4_ping (pong)"}'
-});
