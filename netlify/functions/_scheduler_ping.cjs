5bbf559874ac3851414632eb4abed9becacead0a
LuisSMelchor <luissmmelchor@gmail.com>
2025-08-23 20:29:08 -0400

    Delete netlify/functions/_scheduler_ping.cjs

--- netlify/functions/_scheduler_ping.cjs
+++
@@ -1,8 +0,0 @@
-// Minimalísimo: no toca event/context, no usa process, siempre JSON plano
-exports.handler = async function() {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: "{\"ok\":true,\"ping\":\"scheduler alive (minimal)\"}"
-  };
-};
